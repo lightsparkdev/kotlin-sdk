aa77a4f936a9dc141cc283bf8a80d170e3516bfa
Jeremy Klein <jklein24@gmail.com>
2023-01-27 15:58:26 -0800

    Delete unused colors

--- androiddemo/src/main/java/com/lightspark/androiddemo/ui/theme/Color.kt
+++ androiddemo/src/main/java/com/lightspark/androiddemo/ui/theme/Color.kt
@@ -14,11 +14,3 @@ val NeutralGrey = Color(0xFF4D4D4D)
 val BackgroundOffWhite = Color(0xFFF7F7F7)
 val SurfaceDarker = Color(0xFFF2F2F2)
 val BackgroundDarkGrey = Color(0xFF272727)
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
