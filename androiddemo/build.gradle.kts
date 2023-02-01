39d330ba4fe45f7972fb489f44ad370360bb1af8
Jeremy Klein <jklein24@gmail.com>
2023-02-01 14:56:04 -0800

    Bump version numbers to 4

--- androiddemo/build.gradle.kts
+++ androiddemo/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lightspark.androiddemo"
         minSdk = 24
         targetSdk = 33
-        versionCode = 3
-        versionName = "0.0.3"
+        versionCode = 4
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
