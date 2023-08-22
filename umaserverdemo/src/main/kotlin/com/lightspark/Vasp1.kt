dfc4bc8a47c1a56138afc670bdb4be0749366710
Jeremy Klein <jklein24@gmail.com>
2023-08-22 11:32:20 -0700

    Finished the payreq handler for vasp1

--- umaserverdemo/src/main/kotlin/com/lightspark/Vasp1.kt
+++ umaserverdemo/src/main/kotlin/com/lightspark/Vasp1.kt
@@ -2,11 +2,17 @@ package com.lightspark
 
 import com.lightspark.sdk.LightsparkCoroutinesClient
 import com.lightspark.sdk.uma.LnurlpResponse
+import com.lightspark.sdk.uma.PayReqResponse
+import com.lightspark.sdk.uma.PayerDataOptions
 import com.lightspark.sdk.uma.UmaProtocolHelper
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.response.respond
 
@@ -16,6 +22,7 @@ class Vasp1(
     private val lightsparkClient: LightsparkCoroutinesClient,
 ) {
     private val httpClient = HttpClient()
+    private val requestDataCache = Vasp1RequestCache()
 
     suspend fun handleClientUmaLookup(call: ApplicationCall): String {
         val receiverAddress = call.parameters["receiver"]
@@ -73,14 +80,14 @@ class Vasp1(
             return "Failed to verify lnurlp response signature."
         }
 
-        // TODO(Jeremy): Save the request info to cache.
+        val callbackUuid = requestDataCache.saveLnurlpResponseData(lnurlpResponse, receiverId, receiverVasp)
 
         call.respond(
             mapOf(
                 "currencies" to lnurlpResponse.currencies,
                 "minSendSats" to lnurlpResponse.minSendable,
                 "maxSendSats" to lnurlpResponse.maxSendable,
-                "callbackUuid" to "TODO",
+                "callbackUuid" to callbackUuid,
                 // You might not actually send this to a client in practice.
                 "isReceiverKYCd" to lnurlpResponse.compliance.isKYCd,
             ),
@@ -95,6 +102,10 @@ class Vasp1(
             call.respond(HttpStatusCode.BadRequest, "Callback UUID not provided.")
             return "Callback UUID not provided."
         }
+        val initialRequestData = requestDataCache.getLnurlpResponseData(callbackUuid) ?: run {
+            call.respond(HttpStatusCode.BadRequest, "Callback UUID not found.")
+            return "Callback UUID not found."
+        }
 
         val amount = call.request.queryParameters["amount"]?.let { it.toLongOrNull() }
         if (amount == null || amount <= 0) {
@@ -107,7 +118,7 @@ class Vasp1(
             call.respond(HttpStatusCode.BadRequest, "Currency code not provided.")
             return "Currency code not provided."
         }
-        val currencyValid = uma.getSupportedCurrencies().any { it.code == currencyCode }
+        val currencyValid = initialRequestData.lnurlpResponse.currencies.any { it.code == currencyCode }
         if (!currencyValid) {
             call.respond(HttpStatusCode.BadRequest, "Currency code not supported.")
             return "Currency code not supported."
@@ -120,10 +131,91 @@ class Vasp1(
             return "Failed to fetch public keys."
         }
 
+        val payer = getPayerProfile(initialRequestData.lnurlpResponse.requiredPayerData)
+        val trInfo = "Here is some fake travel rule info. It's up to you to actually implement this if needed."
+        val payerUtxos = emptyList<String>()
+        val utxoCallback = "/api/lnurl/utxocallback?txid=1234"
+
+        val payReq = try {
+            uma.getPayRequest(
+                receiverEncryptionPubKey = vasp2PubKeys.encryptionPubKey,
+                sendingVaspPrivateKey = config.umaSigningPrivKey,
+                currencyCode = currencyCode,
+                amount = amount,
+                payerIdentifier = payer.identifier,
+                isPayerKYCd = true,
+                utxoCallback = utxoCallback,
+                payerUtxos = payerUtxos,
+                payerName = payer.name,
+                payerEmail = payer.email,
+            )
+        } catch (e: Exception) {
+            call.respond(HttpStatusCode.InternalServerError, "Failed to generate payreq.")
+            return "Failed to generate payreq."
+        }
+
+        val response = httpClient.post(initialRequestData.lnurlpResponse.callback) {
+            contentType(ContentType.Application.Json)
+            setBody(payReq)
+        }
+
+        if (response.status != HttpStatusCode.OK) {
+            call.respond(HttpStatusCode.InternalServerError, "Payreq to vasp2 failed: ${response.status}")
+            return "Payreq to vasp2 failed: ${response.status}"
+        }
+
+        val payReqResponse = try {
+            response.body<PayReqResponse>()
+        } catch (e: Exception) {
+            call.respond(HttpStatusCode.InternalServerError, "Failed to parse payreq response.")
+            return "Failed to parse payreq response."
+        }
+
+        // TODO(Yun): Pre-screen the UTXOs from payreqResponse.compliance.ytxos
+
+        val invoice = try {
+            lightsparkClient.decodeInvoice(payReqResponse.encodedInvoice)
+        } catch (e: Exception) {
+            call.respond(HttpStatusCode.InternalServerError, "Failed to decode invoice.")
+            return "Failed to decode invoice."
+        }
+
+        val newCallbackId = requestDataCache.savePayReqData(
+            encodedInvoice = payReqResponse.encodedInvoice,
+            utxoCallback = utxoCallback,
+            invoiceData = invoice,
+        )
+
+        call.respond(
+            mapOf(
+                "encodedInvoice" to payReqResponse.encodedInvoice,
+                "callbackUuid" to newCallbackId,
+                "amount" to invoice.amount,
+                "conversionRate" to payReqResponse.paymentInfo.multiplier,
+                "currencyCode" to payReqResponse.paymentInfo.currencyCode,
+            ),
+        )
+
         return "OK"
     }
+
+    /**
+     * NOTE: In a real application, you'd want to use the authentication context to pull out this information. It's not
+     * actually always Alice sending the money ;-).
+     */
+    private fun getPayerProfile(requiredPayerData: PayerDataOptions) = PayerProfile(
+        name = if (requiredPayerData.nameRequired) "Alice FakeName" else null,
+        email = if (requiredPayerData.emailRequired) "alice@vasp1.com" else null,
+        identifier = "alice",
+    )
 
     suspend fun handleClientSendPayment(call: ApplicationCall): String {
         return "OK"
     }
 }
+
+private data class PayerProfile(
+    val name: String?,
+    val email: String?,
+    val identifier: String,
+)
