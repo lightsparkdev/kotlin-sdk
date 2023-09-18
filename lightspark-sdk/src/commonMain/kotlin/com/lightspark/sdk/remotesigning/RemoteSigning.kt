50afc331d8432678887f38e7fe9ddb1e4a3f2d65
Jeremy Klein <jklein24@gmail.com>
2023-09-17 23:45:07 -0700

    Add ecdh

--- lightspark-sdk/src/commonMain/kotlin/com/lightspark/sdk/remotesigning/RemoteSigning.kt
+++ lightspark-sdk/src/commonMain/kotlin/com/lightspark/sdk/remotesigning/RemoteSigning.kt
@@ -1,15 +1,23 @@
+@file:OptIn(ExperimentalStdlibApi::class)
+
 package com.lightspark.sdk.remotesigning
 
 import com.lightspark.sdk.LightsparkCoroutinesClient
 import com.lightspark.sdk.core.requester.Query
+import com.lightspark.sdk.crypto.RemoteSigning
+import com.lightspark.sdk.crypto.internal.Network
 import com.lightspark.sdk.graphql.DeclineToSignMessagesMutation
+import com.lightspark.sdk.graphql.UpdateNodeSharedSecretMutation
 import com.lightspark.sdk.model.DeclineToSignMessagesOutput
 import com.lightspark.sdk.model.RemoteSigningSubEventType
+import com.lightspark.sdk.model.UpdateNodeSharedSecretOutput
 import com.lightspark.sdk.model.WebhookEventType
 import com.lightspark.sdk.util.serializerFormat
 import com.lightspark.sdk.webhooks.WebhookEvent
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.decodeFromJsonElement
 import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonPrimitive
 
 suspend fun handleRemoteSigningEvent(
     client: LightsparkCoroutinesClient,
@@ -33,15 +41,15 @@ suspend fun handleRemoteSigningEvent(
         return declineToSignMessages(client, event)
     }
 
-    when (subEventType) {
-        RemoteSigningSubEventType.ECDH -> TODO()
+    return when (subEventType) {
+        RemoteSigningSubEventType.ECDH -> handleEcdh(client, event, seedBytes)
         RemoteSigningSubEventType.GET_PER_COMMITMENT_POINT -> TODO()
         RemoteSigningSubEventType.RELEASE_PER_COMMITMENT_SECRET -> TODO()
         RemoteSigningSubEventType.SIGN_INVOICE -> TODO()
         RemoteSigningSubEventType.DERIVE_KEY_AND_SIGN -> TODO()
         RemoteSigningSubEventType.RELEASE_PAYMENT_PREIMAGE -> TODO()
         RemoteSigningSubEventType.REQUEST_INVOICE_PAYMENT_HASH -> TODO()
-        RemoteSigningSubEventType.FUTURE_VALUE -> TODO()
+        RemoteSigningSubEventType.FUTURE_VALUE -> return "unsupported sub_event_type: $subEventType"
     }
 }
 
@@ -57,7 +65,7 @@ private suspend fun declineToSignMessages(client: LightsparkCoroutinesClient, ev
             Query(
                 DeclineToSignMessagesMutation,
                 {
-                    add("payload_ids", payloadIds)
+                    add("payload_ids", serializerFormat.encodeToString(payloadIds))
                 },
             ) {
                 val declineToSignJson =
@@ -74,4 +82,60 @@ private suspend fun declineToSignMessages(client: LightsparkCoroutinesClient, ev
     }
 
     return "rejected signing"
+}
+
+private suspend fun handleEcdh(client: LightsparkCoroutinesClient, event: WebhookEvent, seedBytes: ByteArray): String {
+    event.assertSubEventType(RemoteSigningSubEventType.ECDH)
+    val peerPubKey = event.data?.get("peer_pub_key")?.jsonPrimitive?.content
+        ?: throw RemoteSigningException("Webhook event is missing peer_pub_key")
+
+    val sharedSecret = try {
+        RemoteSigning.ecdh(seedBytes, event.bitcoinNetwork(), peerPubKey).toHexString()
+    } catch (e: Exception) {
+        throw RemoteSigningException("Error computing ECDH shared secret", cause = e)
+    }
+
+    val result = client.executeQuery(
+        Query(
+            UpdateNodeSharedSecretMutation,
+            {
+                add("node_id", event.entityId)
+                add("shared_secret", sharedSecret)
+            },
+        ) {
+            val updateNodeSharedSecretJson =
+                requireNotNull(it["update_node_shared_secret"]) { "Invalid response for shared secret update" }
+            serializerFormat.decodeFromJsonElement<UpdateNodeSharedSecretOutput>(updateNodeSharedSecretJson)
+        },
+    )
+
+    return "updated shared secret for ${result.nodeId}"
+}
+
+private fun WebhookEvent.assertSubEventType(expectedSubEventType: RemoteSigningSubEventType) {
+    if (eventType != WebhookEventType.REMOTE_SIGNING) {
+        throw RemoteSigningException("Webhook event is not for remote signing: $eventType")
+    }
+    val subEventTypeString = data?.get("sub_event_type")
+        ?: throw RemoteSigningException("Webhook event is missing sub_event_type")
+    val subEventType = try {
+        serializerFormat.decodeFromJsonElement<RemoteSigningSubEventType>(subEventTypeString)
+    } catch (e: Exception) {
+        throw RemoteSigningException("Webhook event has invalid sub_event_type: $subEventTypeString", cause = e)
+    }
+    if (subEventType != expectedSubEventType) {
+        throw RemoteSigningException(
+            "Webhook event has unexpected sub_event_type: $subEventTypeString. Expected: $expectedSubEventType",
+        )
+    }
+}
+
+private fun WebhookEvent.bitcoinNetwork(): Network {
+    val bitcoinNetworkString = data?.get("bitcoin_network")
+        ?: throw RemoteSigningException("Webhook event is missing bitcoin_network")
+    return try {
+        serializerFormat.decodeFromJsonElement<Network>(bitcoinNetworkString)
+    } catch (e: Exception) {
+        throw RemoteSigningException("Webhook event has invalid bitcoin_network: $bitcoinNetworkString", cause = e)
+    }
 }
