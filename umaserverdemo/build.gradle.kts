629ed02217a68ce2f5a6ee7dfd0d3eeaa5042e73
Jeremy Klein <jklein24@gmail.com>
2023-12-28 15:58:07 -0800

    Improve the demo uma vasp to add better non-localhost support, etc.

--- umaserverdemo/build.gradle.kts
+++ umaserverdemo/build.gradle.kts
@@ -1,9 +1,6 @@
-import io.ktor.plugin.features.DockerEnvironmentVariable
-import io.ktor.plugin.features.JreVersion
-
 plugins {
     kotlin("jvm")
-    id("io.ktor.plugin") version "2.3.3"
+    id("io.ktor.plugin") version "2.3.7"
     alias(libs.plugins.kotlinSerialization)
 }
 
