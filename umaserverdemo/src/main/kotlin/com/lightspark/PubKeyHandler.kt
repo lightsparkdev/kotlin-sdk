fbbb530b7cc48753e5fc6900b2457d0c9822f1ab
shreyav <shreya@lightspark.com>
2024-03-11 10:59:00 -0700

    update to cert chain

--- umaserverdemo/src/main/kotlin/com/lightspark/PubKeyHandler.kt
+++ umaserverdemo/src/main/kotlin/com/lightspark/PubKeyHandler.kt
@@ -8,8 +8,8 @@ suspend fun handlePubKeyRequest(call: ApplicationCall, config: UmaConfig): Strin
     val twoWeeksFromNowMs = System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 14
 
     val response = PubKeyResponse(
-        signingCertChain = config.umaSigningCertificate,
-        encryptionCertChain = config.umaEncryptionCertificate,
+        signingCertChain = config.umaSigningCertChain,
+        encryptionCertChain = config.umaEncryptionCertChain,
         expirationTs = twoWeeksFromNowMs / 1000,
     )
 
