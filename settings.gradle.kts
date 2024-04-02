c67bc38d50f2ba5e5566a2fa8989b7192818571c
Jeremy Klein <jklein24@gmail.com>
2024-04-02 16:59:17 -0700

    Also remove maven local

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 pluginManagement {
     repositories {
-        mavenLocal()
         google()
         gradlePluginPortal()
         mavenCentral()
@@ -10,15 +9,6 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
-        mavenLocal {
-            content {
-                includeGroup("me.uma")
-            }
-            metadataSources {
-                mavenPom()
-                artifact()
-            }
-        }
         google()
         mavenCentral()
     }
