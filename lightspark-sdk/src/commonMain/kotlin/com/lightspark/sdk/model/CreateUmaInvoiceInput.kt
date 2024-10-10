86e830e1a621e08f1d64acd148d785a9c4aee613
Jason Wang <jason@lightspark.com>
2024-10-10 09:13:13 -0700

    Regenerating SDKs following introspection of schemas.

--- lightspark-sdk/src/commonMain/kotlin/com/lightspark/sdk/model/CreateUmaInvoiceInput.kt
+++ lightspark-sdk/src/commonMain/kotlin/com/lightspark/sdk/model/CreateUmaInvoiceInput.kt
@@ -8,6 +8,11 @@ import kotlinx.serialization.Serializable
 
 /**
  *
+ * @param nodeId The node from which to create the invoice.
+ * @param amountMsats The amount for which the invoice should be created, in millisatoshis.
+ * @param metadataHash The SHA256 hash of the UMA metadata payload. This will be present in the h-tag (SHA256 purpose of payment) of the resulting Bolt 11 invoice.
+ * @param expirySecs The expiry of the invoice in seconds. Default value is 86400 (1 day).
+ * @param receiverHash An optional, monthly-rotated, unique hashed identifier corresponding to the receiver of the payment.
  */
 @Serializable
 @SerialName("CreateUmaInvoiceInput")
