22a8c3d16d432b2598921254485dc6725333b541
Jeremy Klein <jklein24@gmail.com>
2025-02-12 12:43:55 -0800

    Add the outgoing_payment_for_idempotency_key query. (#213)

---
+++ lightspark-sdk/src/commonMain/kotlin/com/lightspark/sdk/graphql/OutgoingPaymentForIdempotencyKey.kt
@@ -0,0 +1,20 @@
+package com.lightspark.sdk.graphql
+
+import com.lightspark.sdk.model.OutgoingPayment
+
+const val OutgoingPaymentForIdempotencyKeyQuery = """
+query OutgoingPaymentForIdempotencyKey(
+	${'$'}idempotency_key: String!
+) {
+	outgoing_payment_for_idempotency_key(input: {
+		idempotency_key: ${'$'}idempotency_key,
+		statuses: ${'$'}transactionStatuses
+	}) {
+		payment {
+			...OutgoingPaymentFragment
+		}
+	}
+}
+
+  ${OutgoingPayment.FRAGMENT}
+"""
